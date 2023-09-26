bd60b1c39000ab806916723b3556ee3e2bb6df63
YasevichAlexandr <114285174+YasevichAlexandr@users.noreply.github.com>
2023-09-26 18:53:03 +0300

    Add files via upload

---
+++ DiagonalMatrix.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace DiagonalMatrix
+{
+    public class DiagonalMatrix<T>
+    {
+        private T[] elements;
+        private int size;
+
+        public event Action<int, int, T> ElementChanged;
+
+        public DiagonalMatrix(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException("Size must be a positive integer.");
+
+            this.size = size;
+            elements = new T[size];
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 || i >= size || j < 0 || j >= size)
+                    throw new IndexOutOfRangeException();
+
+                return i == j ? elements[i] : default(T);
+            }
+            set
+            {
+                if (i < 0 || i >= size || j < 0 || j >= size)
+                    throw new IndexOutOfRangeException();
+
+                T oldValue = i == j ? elements[i] : default(T);
+                if (!Equals(oldValue, value))
+                {
+                    elements[i] = value;
+                    ElementChanged?.Invoke(i, j, oldValue);
+                }
+            }
+        }
+    }
+}
---
+++ SquareMatrix.cs
@@ -0,0 +1,42 @@
+﻿namespace SquareMatrix
+{
+    public class SquareMatrix<T>
+    {
+        private T[] elements;
+        private int size;
+
+        public event Action<int, int, T> ElementChanged;
+
+        public SquareMatrix(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException("Size must be a positive integer.");
+
+            this.size = size;
+            elements = new T[size * size];
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 || i >= size || j < 0 || j >= size)
+                throw new IndexOutOfRangeException();
+
+                return elements[i * size + j];
+            }
+            set
+            {
+                if (i < 0 || i >= size || j < 0 || j >= size)
+                    throw new IndexOutOfRangeException();
+
+                T oldValue = elements[i * size + j];
+                if (!Equals(oldValue, value))
+                {
+                    elements[i * size + j] = value;
+                    ElementChanged?.Invoke(i, j, oldValue);
+                }
+            }
+        }
+    }
+}
