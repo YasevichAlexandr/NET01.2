34ffca17ac52e84ebad996ab12b2b91b77f9aabc
YasevichAlexandr <114285174+YasevichAlexandr@users.noreply.github.com>
2023-09-29 17:04:15 +0300

    Add files via upload

---
+++ DiagonalMatrixTests.cs
@@ -0,0 +1,54 @@
+﻿
+using DiagonalMatrix;
+
+namespace TestProject2
+{
+    public class DiagonalMatrixTests
+    {
+        public void DiagonalMatrix_SetInvalidIndex_ThrowsIndexOutOfRangeException()
+        {
+            DiagonalMatrix<double> matrix = new DiagonalMatrix<double>(2);
+
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[-1, 0] = 1.5);
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[0, -1] = 1.5);
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[2, 0] = 1.5);
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[0, 2] = 1.5);
+        }
+
+        public void DiagonalMatrix_SetAndGetElement_ReturnsCorrectValue()
+        {
+            DiagonalMatrix<double> matrix = new DiagonalMatrix<double>(3);
+
+            matrix[0, 0] = 1.1;
+            matrix[1, 1] = 2.2;
+            matrix[2, 2] = 3.3;
+            
+            Assert.AreEqual(1.1, matrix[0, 0]);
+            Assert.AreEqual(0.0, matrix[0, 1]);
+            Assert.AreEqual(0.0, matrix[0, 2]);
+            Assert.AreEqual(0.0, matrix[1, 0]);
+            Assert.AreEqual(2.2, matrix[1, 1]);
+            Assert.AreEqual(0.0, matrix[1, 2]);
+            Assert.AreEqual(0.0, matrix[2, 0]);
+            Assert.AreEqual(0.0, matrix[2, 1]);
+            Assert.AreEqual(3.3, matrix[2, 2]);
+        }
+        public void DiagonalMatrix_ElementChangedEvent_IsRaised()
+        {
+            DiagonalMatrix<double> matrix = new DiagonalMatrix<double>(3);
+            int eventCount = 0;
+
+            matrix.ElementChanged += (i, j, oldValue) =>
+            {
+                eventCount++;
+            };
+
+            matrix[0, 0] = 1.1;
+            matrix[1, 1] = 2.2;
+            matrix[2, 2] = 3.3;
+            matrix[1, 1] = 2.2; // Не генерируется событие, значение осталось неизменным
+
+            Assert.AreEqual(2, eventCount);
+        }
+    }
+}
---
+++ SquareMatrixTests.cs
@@ -0,0 +1,52 @@
+using SquareMatrix;
+
+namespace TestProject2
+{
+    public class SquareMatrixTests
+    {
+  
+        public void SquareMatrix_SetInvalidIndex_ThrowsIndexOutOfRangeException()
+        {
+            SquareMatrix<int> matrix = new SquareMatrix<int>(3);
+
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[-1, 0] = 1);
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[0, -1] = 1);
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[3, 0] = 1);
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[0, 3] = 1);
+        }
+
+        public void SquareMatrix_SetAndGetElement_ReturnsCorrectValue()
+        {
+            SquareMatrix<int> matrix = new SquareMatrix<int>(2);
+
+            matrix[0, 0] = 1;
+            matrix[0, 1] = 2;
+            matrix[1, 0] = 3;
+            matrix[1, 1] = 4;
+
+            Assert.AreEqual(1, matrix[0, 0]);
+            Assert.AreEqual(2, matrix[0, 1]);
+            Assert.AreEqual(3, matrix[1, 0]);
+            Assert.AreEqual(4, matrix[1, 1]);
+        }
+
+
+        public void SquareMatrix_ElementChangedEvent_IsRaised()
+        {
+            SquareMatrix<int> matrix = new SquareMatrix<int>(2);
+            int eventCount = 0;
+
+            matrix.ElementChanged += (i, j, oldValue) =>
+            {
+                eventCount++;
+            };
+
+            matrix[0, 0] = 1;
+            matrix[1, 1] = 2;
+            matrix[0, 1] = 3; // Генерируется событие
+            matrix[0, 1] = 3; // Не генерируется событие, значение осталось неизменным
+
+            Assert.AreEqual(1, eventCount);
+        }
+    }
+}
